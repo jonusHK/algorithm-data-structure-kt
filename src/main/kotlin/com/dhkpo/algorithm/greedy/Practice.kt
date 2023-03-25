9e04b6e979e11d609f6afb36de0ddfdd1c1e9812
jonusHK <positipman@gmail.com>
2023-03-26 00:16:27 +0900

    Add: Greedy 알고리즘

---
+++ src/main/kotlin/com/dhkpo/algorithm/greedy/Practice.kt
@@ -0,0 +1,38 @@
+package com.dhkpo.algorithm.greedy
+
+
+fun main(args: Array<String>) {
+    val craneCnt = readln().toInt()
+    val craneWeight = readln().split(" ").map { it.toInt() }.toMutableList()
+    val boxCnt = readln().toInt()
+    val boxWeight = readln().split(" ").map { it.toInt() }.toMutableList()
+
+    craneWeight.sort()
+    craneWeight.reverse()
+
+    boxWeight.sort()
+    boxWeight.reverse()
+
+    if (boxWeight[0] > craneWeight[0]) {
+        println(-1)
+        return
+    }
+
+    var cnt = 0
+
+    while (boxWeight.isNotEmpty()) {
+        var boxIdx = 0
+        var craneIdx = 0
+        while (craneIdx < craneCnt) {
+            if (boxIdx == boxWeight.size) break
+            if (craneWeight[craneIdx] >= boxWeight[boxIdx]) {
+                boxWeight.removeAt(boxIdx)
+                craneIdx++
+            } else {
+                boxIdx++
+            }
+        }
+        cnt++
+    }
+    println(cnt)
+}
